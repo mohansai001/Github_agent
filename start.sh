d4a4cc5a0db3c0050c86c241494d7108685a87df
RAGHAVENDRA-VAM <raghavendra.lakkamaraju@valuemomentum.com>
2026-06-22 12:12:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "Starting GitHub MCP Server..."
+echo "Checking environment variable..."
+echo "Azure_Secrets_URL=$Azure_Secrets_URL"
 
+echo "Starting GitHub MCP Server..."
 npx @modelcontextprotocol/server-github &
 
 echo "Starting FastAPI..."
-
 uvicorn main:app --host 0.0.0.0 --port 8000
